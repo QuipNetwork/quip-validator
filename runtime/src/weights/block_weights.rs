546396650b04686344e9d5e3f90a8c176b8181e1
Roman Useinov <roman.useinov@gmail.com>
2026-08-05 08:34:15 +0200

    fmt

--- runtime/src/weights/block_weights.rs
+++ runtime/src/weights/block_weights.rs
@@ -1,4 +1,3 @@
-
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 54.0.0
 //! DATE: 2026-08-04 (Y/M/D)
 //! HOSTNAME: `runner-qkwqxw0vd-project-80034548-concurrent-0`, CPU: `AMD Ryzen 7 3700X 8-Core Processor`
@@ -26,43 +25,43 @@ use sp_core::parameter_types;
 use sp_weights::{constants::WEIGHT_REF_TIME_PER_NANOS, Weight};
 
 parameter_types! {
-	/// Weight of executing an empty block.
-	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
-	///
-	/// Stats nanoseconds:
-	///   Min, Max: 330_168, 344_235
-	///   Average:  335_386
-	///   Median:   334_828
-	///   Std-Dev:  3034.14
-	///
-	/// Percentiles nanoseconds:
-	///   99th: 342_101
-	///   95th: 340_368
-	///   75th: 337_813
-	pub const BlockExecutionWeight: Weight =
-		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(335_386), 0);
+    /// Weight of executing an empty block.
+    /// Calculated by multiplying the *Average* with `1.0` and adding `0`.
+    ///
+    /// Stats nanoseconds:
+    ///   Min, Max: 330_168, 344_235
+    ///   Average:  335_386
+    ///   Median:   334_828
+    ///   Std-Dev:  3034.14
+    ///
+    /// Percentiles nanoseconds:
+    ///   99th: 342_101
+    ///   95th: 340_368
+    ///   75th: 337_813
+    pub const BlockExecutionWeight: Weight =
+        Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(335_386), 0);
 }
 
 #[cfg(test)]
 mod test_weights {
-	use sp_weights::constants;
+    use sp_weights::constants;
 
-	/// Checks that the weight exists and is sane.
-	// NOTE: If this test fails but you are sure that the generated values are fine,
-	// you can delete it.
-	#[test]
-	fn sane() {
-		let w = super::BlockExecutionWeight::get();
+    /// Checks that the weight exists and is sane.
+    // NOTE: If this test fails but you are sure that the generated values are fine,
+    // you can delete it.
+    #[test]
+    fn sane() {
+        let w = super::BlockExecutionWeight::get();
 
-		// At least 100 µs.
-		assert!(
-			w.ref_time() >= 100u64 * constants::WEIGHT_REF_TIME_PER_MICROS,
-			"Weight should be at least 100 µs."
-		);
-		// At most 50 ms.
-		assert!(
-			w.ref_time() <= 50u64 * constants::WEIGHT_REF_TIME_PER_MILLIS,
-			"Weight should be at most 50 ms."
-		);
-	}
+        // At least 100 µs.
+        assert!(
+            w.ref_time() >= 100u64 * constants::WEIGHT_REF_TIME_PER_MICROS,
+            "Weight should be at least 100 µs."
+        );
+        // At most 50 ms.
+        assert!(
+            w.ref_time() <= 50u64 * constants::WEIGHT_REF_TIME_PER_MILLIS,
+            "Weight should be at most 50 ms."
+        );
+    }
 }
